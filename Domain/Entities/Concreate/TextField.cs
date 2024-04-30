0e9d151350b7cc407125cad4edc399651e64f9d4
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-30 22:51:52 +0300

    Add text field class for saving page information in DB

---
+++ Domain/Entities/Concreate/TextField.cs
@@ -0,0 +1,17 @@
+﻿using Appointment_calendar.Domain.Entities.Abstract;
+using System.ComponentModel.DataAnnotations;
+
+namespace Anppointment_calendar.Domain.Entities.Concreate
+{
+    public class TextField : PageEntityBase
+	{
+        [Required]
+        public string CodeWord { get; set; }
+
+        [Display(Name = "Название страницы (заголовок)")]
+        public override string Title { get; set; } = "Информационная страница";
+
+        [Display(Name = "Cодержание страницы")]
+        public override string Text { get; set; } = "Содержание заполняется администратором";
+    }
+}
