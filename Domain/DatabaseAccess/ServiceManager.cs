478ff5f35eda1bc96b7d1c8effa61d8c4e8820ff
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-30 23:13:43 +0300

    Add support class Service manager

---
+++ Domain/DatabaseAccess/ServiceManager.cs
@@ -0,0 +1,19 @@
+﻿using Appointment_calendar.Domain.ServicesRepository.Abstract;
+
+
+
+namespace Appointment_calendar.Domain.DatabaseAccess
+{
+    public class ServiceManager
+    {
+        public ITextFieldsService TextFields { get; set; }
+
+
+        public ServiceManager(ITextFieldsService textFieldsRepository)
+        {
+            TextFields = textFieldsRepository;
+
+
+        }
+    }
+}
