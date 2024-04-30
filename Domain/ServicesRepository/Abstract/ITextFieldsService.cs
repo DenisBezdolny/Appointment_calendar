0130c7fb1452e63c6845f8a2c01692134c0a6250
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-30 23:08:01 +0300

    Create TextField Service

---
+++ Domain/ServicesRepository/Abstract/ITextFieldsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Appointment_calendar.Domain.Entities.Concreate;
+
+namespace Appointment_calendar.Domain.ServicesRepository.Abstract
+{
+    public interface ITextFieldsService
+    {
+        IQueryable<TextField> GetTextFields();
+        TextField GetTextFieldById(Guid id);
+        TextField GetTextFieldByCodeWord(string codeWord);
+        void SaveTextField(TextField entity);
+        void DeleteTextField(Guid id);
+    }
+}
---
+++ Domain/ServicesRepository/Entity Framework/EFTextFieldsRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Appointment_calendar.Domain.Entities.Concreate;
+using Appointment_calendar.Domain.ServicesRepository.Abstract;
+using Appointment_calendar.Domain.DatabaseAccess;
+
+namespace Appointment_calendar.Domain.ServicesRepository.Entity_Framework
+{
+    public class EFTextFieldsService : ITextFieldsService
+    {
+        private readonly AppDbContext context;
+        public EFTextFieldsService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<TextField> GetTextFields()
+        {
+            return context.TextFields;
+        }
+
+        public TextField GetTextFieldById(Guid id)
+        {
+            return context.TextFields.FirstOrDefault(x => x.Id == id);
+        }
+
+        public TextField GetTextFieldByCodeWord(string codeWord)
+        {
+            return context.TextFields.FirstOrDefault(x => x.CodeWord == codeWord);
+        }
+
+        public void SaveTextField(TextField entity)
+        {
+            if (entity.Id == default)
+                context.Entry(entity).State = EntityState.Added;
+            else
+                context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeleteTextField(Guid id)
+        {
+            context.TextFields.Remove(new TextField() { Id = id });
+            context.SaveChanges();
+        }
+    }
+}
