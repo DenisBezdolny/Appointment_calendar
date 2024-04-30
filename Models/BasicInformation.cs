380afb577fb4694203925f0d7868330311354f0c
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-30 22:50:05 +0300

    Add escort class with basic information

---
+++ Models/BasicInformation.cs
@@ -0,0 +1,12 @@
+﻿namespace Appointment_calendar.Models
+{
+	public class BasicInformation
+	{
+		public static string ConnectionString { get; set; }
+		public static string CompanyName { get; set; }
+		public static string CompanyPhone { get; set; }
+		public static string CompanyPhoneShort { get; set; }
+		public static string CompanyEmail { get; set; }
+		public static string CompanyTelegram { get; set;}
+	}
+}
