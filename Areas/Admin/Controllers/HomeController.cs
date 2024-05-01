e8c5500b368e95301fad7496c64ec6dfabbc7cf6
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-01 18:13:15 +0300

    Add admin HomeController

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Appointment_calendar.Domain.DatabaseAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointment_calendar.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class HomeController : Controller
+	{
+		private readonly ServiceManager serviceManager;
+
+		public HomeController(ServiceManager serviceManager)
+		{
+			this.serviceManager = serviceManager;
+		}
+
+        public IActionResult Index()
+        {
+            return View(serviceManager.ServiceItems.GetServiceItems());
+        }
+    }
+
+}
