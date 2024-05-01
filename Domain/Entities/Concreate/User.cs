be3cefdd23d5749caad9c68bee6446a0cddb4bd7
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-01 17:03:12 +0300

    Change User model

--- Domain/Entities/Concreate/User.cs
+++ Domain/Entities/Concreate/User.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Appointment_calendar.Domain.Entities.Concreate
 {
 	public class User : IdentityUser
 	{
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
+
+
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
 		public DateTime? DateOfBirth { get; set; }
+		
 
 	}
 }
