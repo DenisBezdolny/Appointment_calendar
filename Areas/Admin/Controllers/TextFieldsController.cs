39fe4de454a4ce81793f24aacfcb0a361a37f515
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-01 18:13:53 +0300

    Add TextFieldController for editing page information

---
+++ Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,36 @@
+﻿using Appointment_calendar.Domain.DatabaseAccess;
+using Appointment_calendar.Domain.Entities.Concreate;
+using Appointment_calendar.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointment_calendar.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class TextFieldsController : Controller
+	{
+		private readonly ServiceManager serviceManager;
+		public TextFieldsController(ServiceManager serviceManager)
+		{
+			this.serviceManager = serviceManager;
+		}
+
+		public IActionResult Edit(string codeWord)
+		{
+			var entity = serviceManager.TextFields.GetTextFieldByCodeWord(codeWord);
+			return View(entity);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(TextField model)
+		{
+			if (ModelState.IsValid)
+			{
+				serviceManager.TextFields.SaveTextField(model);
+				return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+			}
+			return View(model);
+		}
+	}
+
+}
