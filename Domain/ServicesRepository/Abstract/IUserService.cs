2e134a712cf6c691b76f33c3df388ec36888a5fd
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-01 18:08:51 +0300

    Add UserService for CRUD-operations

---
+++ Domain/ServicesRepository/Abstract/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Appointment_calendar.Domain.Entities.Concreate;
+
+namespace Appointment_calendar.Domain.ServicesRepository.Abstract
+{
+	public interface IUserService
+	{
+		public void CreateUser(User user);
+		public List<User> GetUsers();
+		public User GetUserById(string id);
+		public bool UpdateUser(User user);
+		public bool DeleteUser(string userId);
+
+
+
+	}
+}
---
+++ Domain/ServicesRepository/Entity Framework/EFUserService.cs
@@ -0,0 +1,69 @@
+﻿using Appointment_calendar.Domain.Entities.Concreate;
+using Appointment_calendar.Domain.ServicesRepository.Abstract;
+using Microsoft.AspNetCore.Identity;
+
+namespace Appointment_calendar.Domain.ServicesRepository.Entity_Framework
+{
+	public class EFUserService : IUserService
+	{
+		private readonly UserManager<User> userManager;
+
+		public EFUserService(UserManager<User> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public void CreateUser(User user)
+		{
+			userManager.CreateAsync(user).Wait();
+		}
+
+		public List<User> GetUsers()
+		{
+			return userManager.Users.ToList();
+		}
+		public User GetUserById(string id)
+		{
+			return userManager.FindByIdAsync(id).Result;
+		}
+
+
+		public bool UpdateUser(User user)
+		{
+			try
+			{
+				userManager.UpdateAsync(user).Wait();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public bool DeleteUser(string userId)
+		{
+			var user = userManager.FindByIdAsync(userId).Result;
+			if (user != null)
+			{
+				try
+				{
+					userManager.DeleteAsync(user).Wait();
+					return true;
+				}
+				catch
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+	}
+
+}
+
