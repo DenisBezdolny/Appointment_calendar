941c215d34cd0789c32c375b10fb50993288e41d
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-02 16:11:53 +0300

    add therapist's home controller and view

---
+++ Areas/Therapist/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Appointment_calendar.Domain.DatabaseAccess;
+using Appointment_calendar.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointment_calendar.Areas.Therapist.Controllers
+{
+	[Area("Therapist")]
+	public class HomeController : Controller
+	{
+		private readonly ServiceManager serviceManager;
+
+		public HomeController(ServiceManager serviceManager)
+		{
+			this.serviceManager = serviceManager;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		
+
+        
+    }
+}
