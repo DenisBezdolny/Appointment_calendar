fbc05690369336490cdb1563443dfb8e5bf6fe67
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-02 16:11:07 +0300

    Add controller to display and remove client's application, and add view

---
+++ Areas/Therapist/Controllers/ClientDataApplicationController.cs
@@ -0,0 +1,35 @@
+﻿using Appointment_calendar.Domain.DatabaseAccess;
+using Appointment_calendar.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointment_calendar.Areas.Therapist.Controllers
+{
+
+    [Area("Therapist")]
+    public class ClientDataApplicationController : Controller
+    {
+        private readonly ServiceManager serviceManager;
+
+        public ClientDataApplicationController(ServiceManager serviceManager)
+        {
+            this.serviceManager = serviceManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(serviceManager.ClientDataFields.GetClientDataFields());
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Guid id)
+        {
+            serviceManager.ClientDataFields.DeleteClientDataField(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+        }
+
+
+
+    }
+}
