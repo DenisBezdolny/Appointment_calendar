0862fb79c897d09c4405cf68968cc3b240bfac25
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-02 16:08:36 +0300

    Add registration of user service in serviceManager

--- Domain/DatabaseAccess/ServiceManager.cs
+++ Domain/DatabaseAccess/ServiceManager.cs
@@ -10,12 +10,16 @@ namespace Appointment_calendar.Domain.DatabaseAccess
         public ITextFieldsService TextFields { get; set; }
         public IServiceItemService ServiceItems { get; set; }   
         public IClientDataFieldsService ClientDataFields { get; set; }
+        public IUserService UserServices { get; set; }
 
-        public ServiceManager(ITextFieldsService textFieldsRepository, IServiceItemService serviceItemService, IClientDataFieldsService clientDataFieldsService)
+        public ServiceManager(ITextFieldsService textFieldsRepository, 
+            IServiceItemService serviceItemService, IClientDataFieldsService clientDataFieldsService,
+            IUserService userService)
         {
             TextFields = textFieldsRepository;
             ServiceItems = serviceItemService;
             ClientDataFields = clientDataFieldsService;
+            UserServices = userService;
 
         }
     }
