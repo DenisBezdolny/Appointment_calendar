b7d6cfb0666afb5963aaf5bd1b2c5656bf9a1437
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-02 16:05:17 +0300

    Create ClientDataField controller

---
+++ Controllers/ClientDataFieldController.cs
@@ -0,0 +1,40 @@
+﻿using Appointment_calendar.Domain.DatabaseAccess;
+using Appointment_calendar.Domain.Entities.Concreate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Language_School.Controllers
+{
+    public class ClientDataFieldController : Controller
+    {
+		private readonly ServiceManager serviceManager;
+
+		public ClientDataFieldController(ServiceManager serviceManager)
+        {
+            this.serviceManager = serviceManager;
+        }
+
+
+        public IActionResult Index(Guid id)
+        {
+			var entity = serviceManager.TextFields.GetTextFieldByCodeWord("PageClientDataField");
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Send(ClientDataField model)
+        {
+            if (ModelState.IsValid)
+            {
+				serviceManager.ClientDataFields.SaveClientDataField(model);
+                return RedirectToAction("Complete");
+            }
+            return View();
+        }
+        public IActionResult Complete()
+        {
+            ViewBag.Message = "Ваша заявка принята";
+            return View();
+        }
+    }
+}
