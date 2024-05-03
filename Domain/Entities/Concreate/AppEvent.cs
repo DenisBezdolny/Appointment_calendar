bc440224b00fbe6c73074cb7909c8a04eb9559a6
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-03 23:00:47 +0300

    add event model (alpha version)

---
+++ Domain/Entities/Concreate/AppEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appointment_calendar.Domain.Entities.Concreate
+{
+    public class AppEvent
+    {
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Start { get; set; }
+
+        [Required]
+        public string End { get; set; }
+
+        [Required]
+        public User? User { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+
+    }
+}
