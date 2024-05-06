20dda5f4e125ea689eb0800d618c76a556da9848
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-06 12:12:02 +0300

    add AppEvent service

---
+++ Domain/ServicesRepository/Abstract/IAppEventService.cs
@@ -0,0 +1,12 @@
+﻿using Appointment_calendar.Domain.Entities.Concreate;
+
+namespace Appointment_calendar.Domain.ServicesRepository.Abstract
+{
+    public interface IAppEventService
+    {
+        IQueryable<AppEvent> GetEvents();
+        AppEvent GetEventById(string id);
+        void SaveEvent(AppEvent entity);
+        void DeleteEvent(string id);
+    }
+}
---
+++ Domain/ServicesRepository/Entity Framework/EFAppEventService.cs
@@ -0,0 +1,41 @@
+﻿using Appointment_calendar.Domain.DatabaseAccess;
+using Appointment_calendar.Domain.Entities.Concreate;
+using Appointment_calendar.Domain.ServicesRepository.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace Appointment_calendar.Domain.ServicesRepository.Entity_Framework
+{
+    public class EFAppEventService : IAppEventService
+    {
+        private readonly AppDbContext context;
+        public EFAppEventService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<AppEvent> GetEvents()
+        {
+            return context.AppEvents;
+        }
+
+        public AppEvent GetEventById(string id)
+        {
+            return context.AppEvents.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void SaveEvent(AppEvent entity)
+        {
+            if (entity.Id != default)
+                context.Entry(entity).State = EntityState.Added;
+            else
+                context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeleteEvent(string id)
+        {
+            context.AppEvents.Remove(new AppEvent() { Id = id });
+            context.SaveChanges();
+        }
+    }
+}
