3f3a7046cf9c1b0ca05af75edd7972aa81532fc0
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-06 13:48:27 +0300

    Add service controller in admin area

---
+++ Areas/Admin/Controllers/ServiceItemsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Appointment_calendar.Domain.DatabaseAccess;
+using Appointment_calendar.Domain.Entities.Concreate;
+using Appointment_calendar.Services;
+using Appointment_calendar.Domain.DatabaseAccess;
+
+namespace Appointment_calendar.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceItemsController : Controller
+    {
+        private readonly ServiceManager serviceManager;
+        private readonly IWebHostEnvironment hostingEnvironment;
+        public ServiceItemsController(ServiceManager serviceManager, IWebHostEnvironment hostingEnvironment)
+        {
+            this.serviceManager = serviceManager;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            var entity = id == default ? new ServiceItem() : serviceManager.ServiceItems.GetServiceItemById(id);
+            return View(entity);
+        }
+        [HttpPost]
+        public IActionResult Edit(ServiceItem model, IFormFile titleImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (titleImageFile != null)
+                {
+                    model.TitleImagePath = titleImageFile.FileName;
+                    using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath, "images/", titleImageFile.FileName), FileMode.Create))
+                    {
+                        titleImageFile.CopyTo(stream);
+                    }
+                }
+                serviceManager.ServiceItems.SaveServiceItem(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Guid id)
+        {
+            serviceManager.ServiceItems.DeleteServiceItem(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+        }
+    }
+}
