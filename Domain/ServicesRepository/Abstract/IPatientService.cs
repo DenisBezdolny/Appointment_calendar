2b858333c601e692ed63182577c78a3f4d719418
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-05-06 12:11:25 +0300

    change user service to patient service

---
+++ Domain/ServicesRepository/Abstract/IPatientService.cs
@@ -0,0 +1,17 @@
+﻿using Appointment_calendar.Domain.Entities.Concreate;
+
+namespace Appointment_calendar.Domain.ServicesRepository.Abstract
+{
+	public interface IPatientService
+	{
+		public void CreatePatient(Patient patient);
+		public IQueryable<Patient> GetPatients();
+        public Patient GetPatientById(string id);
+		public bool UpdatePatient(Patient patient);
+		public void DeletePatient(string id);
+
+
+
+
+    }
+}
---
+++ Domain/ServicesRepository/Entity Framework/EFPatientService.cs
@@ -0,0 +1,59 @@
+﻿using Appointment_calendar.Domain.DatabaseAccess;
+using Appointment_calendar.Domain.Entities.Concreate;
+using Appointment_calendar.Domain.ServicesRepository.Abstract;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using System.Linq;
+
+namespace Appointment_calendar.Domain.ServicesRepository.Entity_Framework
+{
+	public class EFPatientService : IPatientService
+	{
+        private readonly AppDbContext context;
+
+        public EFPatientService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreatePatient(Patient patient)
+        {
+            if (patient.Id != default)
+            {
+                new IdentityUserRole<string>() { RoleId = "ee560ba8-3277-4414-9a66-300fb2ffae38", UserId = patient.Id };
+
+                context.Entry(patient).State = EntityState.Added;
+            }    
+                
+            else
+                context.Entry(patient).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeletePatient(string id)
+        {
+            var patient = context.Patients.FirstOrDefault(x => x.Id == id);
+            context.Patients.Remove(patient);
+            context.SaveChanges();
+        }
+
+        public Patient GetPatientById(string id)
+        {
+            return context.Patients.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<Patient> GetPatients()
+        {
+            return context.Patients;
+        }
+
+        public bool UpdatePatient(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
+
